35997661172bf45fc4e4562c4e5b25f581b8c4e3
Paulo Guevara <pguevara135@hotmail.com>
2024-10-14 15:02:19 -0300

    Remanufaturing the function to change screen size

--- Exercicios/ex027/flex013 -menu/script.js
+++ Exercicios/ex027/flex013 -menu/script.js
@@ -2,27 +2,22 @@
     let menuMobileClose = document.getElementById('close');
     let navItem = document.getElementById('nav-item');
 
-    window.addEventListener('resize', function() {
-        var largura = window.innerWidth;
-        if (largura > 600) {
-            menuClose()
+    window.addEventListener('resize', menuClose)
+    menuClose()
+    function menuShow() {
+        
+        if (menuMobileOpen.classList.contains('open')) {
+            menuMobileOpen.classList.remove('open');
+            menuMobileClose.classList.add('close');
+            navItem.classList.remove('nav-item');
         }
-    })
-
-function menuShow() {
-    
-    if (menuMobileOpen.classList.contains('open')) {
-        menuMobileOpen.classList.remove('open');
-        menuMobileClose.classList.add('close');
-        navItem.classList.remove('nav-item');
     }
-}
 
-function menuClose() {
+    function menuClose() {
 
-    if (menuMobileClose.classList.contains('close')) {
-        menuMobileOpen.classList.add('open');
-        menuMobileClose.classList.remove('close');
-        navItem.classList.add('nav-item');
-    }
+        if (menuMobileClose.classList.contains('close')) {
+            menuMobileOpen.classList.add('open');
+            menuMobileClose.classList.remove('close');
+            navItem.classList.add('nav-item');
+        }
 }
